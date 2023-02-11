fc05ed659fc3c5066c3b2652f0ad6bd027a13c04
waflol <clark.le.goldenowl@gmail.com>
2023-02-11 23:30:33 +0700

    check env ci

--- src/app/App.js
+++ src/app/App.js
@@ -6,7 +6,7 @@ import React from 'react';
 function App() {
     const [ data, setData ] = useState(null);
     useEffect(() => {
-        fetch('http://localhost:8000/test/')
+        fetch(`${process.env.HOST_API}/test/`)
             .then((res) => res.json())
             .then((data) => setData(data.data));
     });
