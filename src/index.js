66eec57c2889806a9692b36110e19265a992e5f3
waflol <clark.le.goldenowl@gmail.com>
2023-02-13 03:35:40 +0700

    SC-4 update package sentry

--- src/index.js
+++ src/index.js
@@ -2,6 +2,20 @@ import App from '@app/App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+Sentry.init({
+    dsn: `${process.env.REACT_APP_SENTRY_HOST}`,
+    integrations: [ new BrowserTracing() ],
+  
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+});
+
+Sentry.captureMessage("this is a debug message", "debug");
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
