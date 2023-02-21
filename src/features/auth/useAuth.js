46adb5306e7e48754e0e742da9c1224033482584
Ngọc Đức <70329271+ngocducpham@users.noreply.github.com>
2023-02-21 09:42:21 +0700

    finished setup

--- src/features/auth/useAuth.js
+++ src/features/auth/useAuth.js
@@ -1,4 +1,4 @@
-import { removeItem, setData } from '@common/utils/localStorage';
+import { getData, removeItem, setData } from '@common/utils/localStorage';
 import { storageKeyAccessToken, storageKeyRefreshToken } from '@constants';
 import api, { endPoints, publicApi } from '@services/api';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
@@ -12,8 +12,8 @@ function useAuth() {
         error: profileError,
     } = useQuery({
         queryKey: [ 'profile/me' ],
-        queryFn: ({ signal }) => {
-            return api(endPoints.user.profile, { signal });
+        queryFn: () => {
+            return api(endPoints.user.profile);
         },
         retryOnMount: false,
     });
