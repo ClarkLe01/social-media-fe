a595ede2aa88065431434719772fed26628de4d9
ClarkLe01 <clark.le.goldenowl@gmail.com>
2023-05-31 15:21:53 +0700

    SC-51 add new

--- src/features/settings/Settings.js
+++ src/features/settings/Settings.js
@@ -117,7 +117,7 @@ function Settings() {
                                         header: 'd-flex justify-content-between',
                                         title: 'flex-fill mx-auto pe-3 my-1',
                                     }}
-                                >
+                                > 
 
                                     <form onSubmit={form.onSubmit(handleChangePassword)}>
                                         <div>
