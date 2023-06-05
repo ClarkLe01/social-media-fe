052d86891b06254e200bc4e04b49ce9828274f42
LamMN1012 <01274757673lam@gmail.com>
2023-06-05 17:44:39 +0700

    SC-61 Done

--- src/index.js
+++ src/index.js
@@ -31,12 +31,12 @@ const queryClient = new QueryClient({
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <QueryClientProvider client={queryClient}>
-                <App />
-                <ReactQueryDevtools initialIsOpen />
-            </QueryClientProvider>
-        </Provider>
-    </React.StrictMode>,
+    // <React.StrictMode>
+    <Provider store={store}>
+        <QueryClientProvider client={queryClient}>
+            <App />
+            <ReactQueryDevtools initialIsOpen />
+        </QueryClientProvider>
+    </Provider>,
+    // </React.StrictMode>,
 );
